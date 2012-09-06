91a32d508cac3528f7ae845573455ec263aa76b1
Chris Sosa <sosa@chromium.org>
2012-09-06 15:35:29 -0700

    Checking in a small tool that has customizable hooks to recover DUTs.
    Checking in a new tool that runs after failsafe.conf has started
    (30s in to boot or after Chrome has booted) that will run a set of hooks
    on Chromium/Chrome OS test images that check "bricked" conditions and fix them.
    I'm starting with a hook that toggles eth0 and restarts shill if we can't reach
    www.google.com.
    
    BUG=chromium-os:34178
    TEST=Ran it and checked logs
    
    Change-Id: I65f1c0eec61ff4dac04478d8bf2e94abd0609dbe
    Reviewed-on: https://gerrit.chromium.org/gerrit/32453
    Commit-Ready: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

---
+++ recover_duts/recover_duts.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This module runs at system startup on Chromium OS test images. It runs through
+# a set of hooks to keep a DUT from being bricked without manual intervention.
+# Example hook:
+#   Check to see if ethernet is connected. If its not, unload and reload the
+#     ethernet driver.
+
+import logging
+import os
+import subprocess
+import time
+
+LOGGING_SUBDIR = '/var/log/recover_duts'
+LOG_FILENAME_FORMAT = os.path.join(LOGGING_SUBDIR,
+                                   'recover_duts_log_%Y%m%d_%H%M%S.txt')
+LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+SLEEP_DELAY = 600
+
+
+def main():
+  if not os.path.isdir(LOGGING_SUBDIR):
+    os.makedirs(LOGGING_SUBDIR)
+
+  log_filename = time.strftime(LOG_FILENAME_FORMAT)
+  logging.basicConfig(filename=log_filename, level=logging.DEBUG,
+                      format=LOGGING_FORMAT)
+  hooks_dir = os.path.join(os.path.dirname(__file__), 'hooks')
+  try:
+    while(True):
+      for script in os.listdir(hooks_dir):
+        script = os.path.join(hooks_dir, script)
+        if os.path.isfile(script) and script.endswith('.hook'):
+          logging.debug('Running hook: %s', script)
+          popen = subprocess.Popen([script], stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+          output = popen.communicate()[0]
+          if popen.returncode == 0:
+            logging.debug('Running of %s succeeded with output:\n%s', script,
+                          output)
+          else:
+            logging.warn('Running of %s failed with output:\n%s', script,
+                         output)
+      else:
+        time.sleep(SLEEP_DELAY)
+
+  except Exception as e:
+    # Since this is run from an upstart job we want to ensure we log this into
+    # our log file before dying.
+    logging.fatal(str(e))
+    raise
+
+
+if __name__ == '__main__':
+  main()
